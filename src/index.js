62e30bb4c0db8722c7c2f0dd2d81d46b6514a422
ys0o8qy <151720307+ys0o8qy@users.noreply.github.com>
2024-08-29 08:44:16 +0000

    change domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.subtitlecraft.com": dockerHub,
+  "quay.subtitlecraft.com": "https://quay.io",
+  "gcr.subtitlecraft.com": "https://gcr.io",
+  "k8s-gcr.subtitlecraft.com": "https://k8s.gcr.io",
+  "k8s.subtitlecraft.com": "https://registry.k8s.io",
+  "ghcr.subtitlecraft.com": "https://ghcr.io",
+  "cloudsmith.subtitlecraft.com": "https://docker.cloudsmith.io",
+  "ecr.subtitlecraft.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.subtitlecraft.com": dockerHub,
 };
 
 function routeByHosts(host) {
